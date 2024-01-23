a11ca08cfd5a750c6b1746d4c50c661ae66637f1
parkgunwook1 <pkw85428@naver.com>
2024-01-23 23:30:26 +0900

    json 응답

--- springmvc/src/main/java/hello/springmvc/basic/request/RequestBodyJsonController.java
+++ springmvc/src/main/java/hello/springmvc/basic/request/RequestBodyJsonController.java
@@ -1,2 +1,89 @@
-package hello.springmvc.basic.request;public class RequestBodyJsonController {
+package hello.springmvc.basic.request;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.springmvc.basic.HelloData;
+import jakarta.servlet.ServletInputStream;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * {"username":"hello", "age":20}
+ * content-type : application/json
+ * */
+@Slf4j
+@Controller
+public class RequestBodyJsonController {
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    /*
+    *
+    *  HttpServletRequest를 사용해서 직접 HTTP 메시지 바디에서 데이터를 읽어와서, 문자로 변환한다.
+    *  문자로 된 JSON 데이터를 Jackson 라이브러리인 objectMapper 를 사용해서 자바 객체로 변환한다.
+    * */
+    @PostMapping("/request-body-json-v1")
+    public void requestBodyJsonV1(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        ServletInputStream inputStream = request.getInputStream();
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+        log.info("messageBody={}", messageBody);
+        HelloData helloData = objectMapper.readValue(messageBody, HelloData.class);
+        log.info("username={}", helloData.getUsername(), helloData.getAge());
+
+        response.getWriter().write("ok");
+    }
+
+    @ResponseBody
+    @PostMapping("/request-body-json-v2")
+    public String requestBodyJsonV2(@RequestBody String messageBody) throws IOException {
+
+        log.info("messageBody={}", messageBody);
+        HelloData helloData = objectMapper.readValue(messageBody, HelloData.class);
+        log.info("username={}", helloData.getUsername(), helloData.getAge());
+
+        return"ok";
+    }
+
+    @ResponseBody
+    @PostMapping("/request-body-json-v3")
+    public String requestBodyJsonV3(@RequestBody HelloData data) throws IOException {
+        log.info("username={}, age={}", data.getUsername(), data.getAge());
+
+        return"ok";
+    }
+
+    @ResponseBody
+    @PostMapping("/request-body-json-v4")
+    public String requestBodyJsonV4(HttpEntity<HelloData> data) throws IOException {
+        HelloData helloData = data.getBody();
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+
+        return"ok";
+    }
+
+    @ResponseBody // 응답의 경우에도 @ResponseBody를 사용하면 해당 객체를 HTTP 메시지 바디에 직접 넣어줄 수 있다.
+    @PostMapping("/request-body-json-v5")
+    public HelloData requestBodyJsonV5(@RequestBody HelloData data) throws IOException {
+        log.info("username={}, age={}", data.getUsername(), data.getAge());
+
+        return data;
+    }
+
+    /*
+    * @RequestBody 요청
+    *   - JSON 요청 > HTTP 메시지 컨버터 > 객체
+    *
+    * @ResponseBody 응답
+    *   - 객체 > HTTP 메시지 컨버터 > JSON 응답
+    * */
 }
