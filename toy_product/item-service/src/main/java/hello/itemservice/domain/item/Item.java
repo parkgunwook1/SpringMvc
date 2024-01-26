9614afc73a88d359408638185b77bf5d50c14aad
parkgunwook1 <pkw85428@naver.com>
2024-01-26 16:59:02 +0900

    상품 비즈니스 로직

--- toy_product/item-service/src/main/java/hello/itemservice/domain/item/Item.java
+++ toy_product/item-service/src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,2 +1,27 @@
-package hello.itemservice.domain.item;public class Item {
+package hello.itemservice.domain.item;
+
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+public class Item {
+
+    private Long id;
+    private String itemName;
+    private Integer price;
+    private Integer quantity;
+
+    public Item() {
+    }
+    public Item(String itemName, Integer price, Integer quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+    // int는 null이 못들어가서 Integer로 사용했다.
+
+    // 데이터가 왔다갔다하는 dto 에서는 @Data 사용가능
+    // @Data를 쓸때는 주의할 필요가있다.
 }
--- toy_product/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ toy_product/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -1,2 +1,46 @@
-package hello.itemservice.web.basic;public class BasicItemController {
+package hello.itemservice.web.basic;
+
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/items") // 클래스 레벨에서 이 클래스에 속한 모든 메서드는 basic/items 경로 매핑
+@RequiredArgsConstructor
+public class BasicItemController {
+
+    private final ItemRepository itemRepository;
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items",items);
+        return "basic/items";
+        // return 해당 메서드가 처리된 후에 클라이언트에게 전달할 뷰의 이름 지정
+    }
+
+    /**
+    * 테스트용 데이터 추가
+     * */
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("itemA", 10000,10));
+        itemRepository.save(new Item("itemB", 20000,20));
+
+
+    }
+
+//    public BasicItemController(ItemRepository itemRepository){
+//        this.itemRepository = itemRepository;
+//    }
+//      @RequiredArgsConstructor 가 생성자 하나일 때 만들어준다.
+
 }
