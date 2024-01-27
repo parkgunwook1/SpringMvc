2ed1adfe379e7547cc5669f3c954af75a4f23037
parkgunwook1 <pkw85428@naver.com>
2024-01-28 01:31:04 +0900

    쿠키/세션 로그인

---
+++ login/src/test/java/hello/login/web/session/SessionManagerTest.java
@@ -0,0 +1,45 @@
+package hello.login.web.session;
+
+import hello.login.domain.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import javax.servlet.http.HttpServletResponse;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SessionManagerTest {
+
+    SessionManager sessionManager = new SessionManager();
+
+    @Test
+    void sessionTest() {
+
+        /*
+        * HttpServletResponse, HttpServletRequest 객체를 직접 사용할 수 없기 때문에 테스트에서 비슷한
+        * 역할을 해주는 가짜 MockHttpServletResponse, MockHttpServletRequest를 사용했다.
+        * */
+
+        // 세션 생성
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        Member member = new Member();
+        sessionManager.createSession(member, response);
+
+        // 요청에 응답 쿠키 저장
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setCookies(response.getCookies());
+
+        // 세션 조회
+        Object result = sessionManager.getSession(request);
+        assertThat(result).isEqualTo(member);
+
+        // 세션 만료
+        sessionManager.expire(request);
+        Object expired = sessionManager.getSession(request);
+        assertThat(result).isEqualTo(expired);
+    }
+
+}
